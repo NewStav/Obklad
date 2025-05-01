4f9f5a724d51935063b2b8e4877caf7c6421b709
NewStav <1ivan1ivan242424@gmail.com>
2025-05-01 10:41:43 +0200

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// ВСТАВ СЮДИ СВІЙ firebaseConfig після створення проекту
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Ініціалізація Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
